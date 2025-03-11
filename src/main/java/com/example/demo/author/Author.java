bbaa031ac6e548029734788608fcf039257c44fc
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-03-11 22:42:57 +0100

    create author class

---
+++ src/main/java/com/example/demo/author/Author.java
@@ -0,0 +1,84 @@
+package com.example.demo.author;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table
+public class Author {
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "book_sequence"
+    )
+
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+    private String nationality;
+    
+    public Author() {
+    
+    }
+
+    public Author(Long id, String name, LocalDate birthDate, String nationality) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+    }
+
+    public Author(String name, LocalDate birthDate, String nationality) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+    }
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", nationality='" + nationality + '\'' +
+                '}';
+    }
+}
